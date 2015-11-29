88dbb4bb3905fe28097ec63afc1f2b31e950d26e
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-29 00:32:33 -0800

    visual effect for winning grid almost done

--- view/js/ttt-view.js
+++ view/js/ttt-view.js
@@ -22,8 +22,8 @@
     var idArr = $square.attr("id").split('-');
     var gridId = idArr[1];
     var id = idArr[2];
-    var gridPos = [Math.floor(gridId/3), (gridId%3)];
-    var pos = [Math.floor(id/3), (id%3)];
+    var gridPos = [Math.floor(gridId / 3), (gridId % 3)];
+    var pos = [Math.floor(id / 3), (id % 3)];
 
     if (this.game.playMove(gridPos, pos)) {
       this.clearMessage();
@@ -35,9 +35,37 @@
 
   View.prototype.updateView = function ($square) {
     var mark = this.game.currentPlayer;
+    var game = this.game;
     $square.html("<span class='mark " + mark + "'>" + mark + "</span>");
     $square.addClass("clicked");
     $square.removeClass("hoverable");
+
+    $('.square').each(function () {
+      var miniSquare = $(this).find('.mini-square');
+      var gridPos = [Math.floor(parseInt(this.id) / 3), Math.floor(parseInt(this.id) % 3)];
+      var miniBoard = game.board.grid[gridPos[0]][gridPos[1]];
+      if (miniBoard.isOver()) {
+        var id = gridPos[0] * 3 + gridPos[1] * 3 - (gridPos[1] * 3 - gridPos[1]);
+        $('#' + id.toString()).addClass('won-grid-' + miniBoard.winner());
+      }
+
+      if (game.board.validGrids.indexOf(gridPos[0] + ',' + gridPos[1]) === -1) {
+        miniSquare.each(function () {
+          this.classList.remove('playable');
+          this.classList.remove('hoverable');
+        })
+      } else {
+        miniSquare.each(function () {
+          this.classList.add('playable');
+          this.classList.add('hoverable');
+        })
+      }
+    })
+
+    $('.clicked').each(function () {
+      this.classList.remove('playable');
+    })
+
     if (this.game.isOver()) {
       if (this.game.winner()) {
         // debugger
@@ -60,12 +88,12 @@
   };
 
   View.prototype.setupBoard = function () {
-    var $elem = $('<ul class="board"></ul>');
+    var $elem = $('<ul id="board"></ul>');
 
     this.$el.append($elem);
     var toInsert = "";
     for (var i = 0; i < 9; i++) {
-      toInsert = toInsert + "<li class='square grid hoverable' id=" + i + ">" + this.setupMiniBoard(i) + "</li>";
+      toInsert = toInsert + "<li class='square grid' id=" + i + ">" + this.setupMiniBoard(i) + "</li>";
     }
     $elem.html(toInsert);
   };
